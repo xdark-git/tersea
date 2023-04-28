5f93a05290999966f06e1e646597da200b99c9c3
xdark-git <konteyemouhamadou@gmail.com>
2023-04-28 01:09:37 +0100

    adding seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,14 +3,20 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
+use App\Models\Status;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         // \App\Models\User::factory(10)->create();
 
@@ -18,5 +24,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Admin::create([
+            'name' => $faker->name(),
+            'email' => 'admin@tersea.com',
+            'password' => Hash::make('tersea'),
+        ]);
+
+        Status::factory()
+            ->count(3)
+            ->state(new Sequence(
+                ['name' => 'En attente' ],
+                ['name' => 'Valider' ],
+                ['name' => 'Annuler' ],
+            ))
+            ->create();
     }
 }
