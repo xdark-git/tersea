5fcab91e8f31876c718e4afdfd08ccd0766c6697
xdark-git <konteyemouhamadou@gmail.com>
2023-04-29 20:52:34 +0100

    adding trycatch method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,36 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AdminResource;
 use App\Models\Admin;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class AdminController extends Controller
 {
     public function login(Request $request)
     {
             
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-     
-        $user = Admin::where('email', $request->email)->first();
-     
-        if (! $user || ! Hash::check($request->password, $user->password)) {
-            return Response([
-                'message' => 'The provided credentials are incorrect.']
-                , Response::HTTP_UNAUTHORIZED);
-        }
+        try{
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+         
+            $user = Admin::where('email', $request->email)->first();
+         
+            if (! $user || ! Hash::check($request->password, $user->password)) {
+                return Response([
+                    'message' => 'The provided credentials are incorrect.']
+                    , Response::HTTP_UNAUTHORIZED);
+            }
+    
+            $token = $user->createToken('token',  ['admin'])->plainTextToken;
+         
+           return Response([
+                'data' => AdminResource::make($user),
+                'token' => $token
+            ],Response::HTTP_ACCEPTED);
 
-        $token = $user->createToken('token')->plainTextToken;
-     
-       return Response([
-        'data' => AdminResource::make($user),
-        'token' => $token
-    ],Response::HTTP_ACCEPTED);
+        }catch(Exception $e){
+
+            return Response([
+                'message' => $e->getMessage()
+            ],Response::HTTP_BAD_REQUEST); 
+        }
         
         
     }
