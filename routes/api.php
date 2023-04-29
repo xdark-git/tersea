a4a90e52050c6c8805ad6de655bf80c0bb4dbeaa
xdark-git <konteyemouhamadou@gmail.com>
2023-04-29 03:33:35 +0100

    new api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/admin/')->group(function(){
+    Route::post('login', [AdminController::class, 'login']);
+    Route::get('getAll', [AdminController::class, 'getAll']);
+    Route::post('add', [AdminController::class, 'add']);
 });
