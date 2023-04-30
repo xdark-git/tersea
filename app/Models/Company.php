477f429022bc731ca1c3e2622907212525c1da04
xdark-git <konteyemouhamadou@gmail.com>
2023-04-30 04:00:46 +0100

    adding fillable

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Company extends Model
 {
     use HasFactory;
+    protected $fillable = [ "name", "admin_id"];
 
     public function admin(): BelongsTo
     {
