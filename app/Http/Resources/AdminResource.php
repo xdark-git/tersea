1b37884820b1742292d864e3ba4d5996a1ff325d
xdark-git <konteyemouhamadou@gmail.com>
2023-04-30 04:00:10 +0100

    refactoring

--- app/Http/Resources/AdminResource.php
+++ app/Http/Resources/AdminResource.php
@@ -21,12 +21,12 @@ class AdminResource extends JsonResource
             'id' => $this->resource->id,
             'name' => $this->resource->name,
             'email' => $this->resource->email,
-            'admin' =>  $this->resource->parent ? [
+            'is_admin' => true,
+            'created_by' =>  $this->resource->parent ? [
                 'id' => $this->resource->parent->id,
                 'name' => $this->resource->parent->name,
                 'email'=> $this->resource->parent->email
             ] : null,
-
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
 
