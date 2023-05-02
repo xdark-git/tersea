3c5267989a93065c409060874a26993d6927adf7
xdark-git <konteyemouhamadou@gmail.com>
2023-05-02 01:55:09 +0100

    api request file

---
+++ resources/js/components/vue/api/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const connectAdmin = (data) => {
+    return api.post("/admin/login", data);
+};
+
+export const connectEmployee = (data) => {
+    return api.post("/employee/login", data);
+};
