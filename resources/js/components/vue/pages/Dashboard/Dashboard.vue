47681346b2556e8937205417af526ae3635720fe
xdark-git <konteyemouhamadou@gmail.com>
2023-05-02 01:55:43 +0100

    building template

---
+++ resources/js/components/vue/pages/Dashboard/Dashboard.vue
@@ -0,0 +1,36 @@
+<template >
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Acceuil</router-link>
+                </li>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Mon Profile</router-link>
+                </li>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Liste des employés</router-link>
+                </li>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Liste des Entreprises</router-link>
+                </li>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Liste des admins</router-link>
+                </li>
+                <li>
+                    <router-link :to="{path:'/'}" class="link-back-homepage">Historique</router-link>
+                </li>
+            </ul>
+            <div class="logout">
+                <button>Se déconnecter</button>
+            </div>
+        </nav>
+        <div class="content">
+            <h1>Bonjour et Bienvenue</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ resources/js/components/vue/pages/NotFound/NotFound.vue
@@ -0,0 +1,10 @@
+<template >
+    <div class="not-found">
+        <div >Page Introuvable</div>
+        <router-link :to="{path:'/'}" class="link-back-homepage">Retour</router-link>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
