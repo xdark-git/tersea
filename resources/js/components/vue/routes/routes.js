062172449439234ceeb4031a398098a374b60dec
xdark-git <konteyemouhamadou@gmail.com>
2023-05-02 01:55:20 +0100

    new routes

--- resources/js/components/vue/routes/routes.js
+++ resources/js/components/vue/routes/routes.js
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginAdmin from "../pages/Admin/LoginAdmin.vue";
+import Login from "../pages/Admin/Login.vue";
 import NotFound from "../pages/NotFound/NotFound.vue";
+import Dashboard from "../pages/Dashboard/Dashboard.vue";
 const routes = [
+    {
+        path: "/",
+        name: "dashboard",
+        component: Dashboard,
+    },
     {
         path: "/admin/login",
         name: "login",
-        component: LoginAdmin,
+        component: Login,
+    },
+    {
+        path: "/employee/login",
+        name: "emp-login",
+        component: Login,
     },
     { path: "/:pathMatch(.*)", name: "NotFound", component: NotFound },
 ];
