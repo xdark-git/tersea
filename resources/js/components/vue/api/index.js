45e3b8c72c4598c5bb1d3b49e7461e48402381ef
xdark-git <konteyemouhamadou@gmail.com>
2023-05-03 20:04:05 +0100

    passing token as authorization if token exist

--- resources/js/components/vue/api/index.js
+++ resources/js/components/vue/api/index.js
@@ -1,10 +1,22 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+const userCookieName = import.meta.env.VITE_VUE_APP_USER_COOKIE;
 
 const api = axios.create({
     baseURL: "/api",
     headers: {
         "Content-Type": "application/json",
     },
+});
+
+api.interceptors.request.use((req) => {
+    const userCookie = Cookies.get(userCookieName);
+
+    if (userCookie) {
+        const userData = JSON.parse(userCookie);
+        req.headers.Authorization = `Bearer ${userData.token}`;
+    }
+    return req;
 });
 
 export const connectAdmin = (data) => {
@@ -17,4 +29,8 @@ export const connectEmployee = (data) => {
 
 export const checkSession = (data) => {
     return api.post("/verify/token", data);
+};
+
+export const endSession = () => {
+    return api.delete("/verify/end-session");
 };
